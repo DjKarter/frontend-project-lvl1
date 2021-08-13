32c3b681549a2a7d1aa6b8e2038e5b641f682080
DjKarter <startzew.dima@mail.ru>
2021-08-13 12:01:18 +0300

    Step 4 one more

--- src/cli.js
+++ src/cli.js
@@ -2,8 +2,10 @@ import readlineSync from 'readline-sync';
 
 const cl = (str) => console.log(str);
 
-export const helloUser = () => {
+const helloUser = () => {
   cl('Welcome to Brain Games!');
   const name = readlineSync.question('Your answer: ');
   cl(`Hello, ${name}!`);
 };
+
+export default helloUser;
